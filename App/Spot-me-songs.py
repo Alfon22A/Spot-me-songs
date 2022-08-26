b2f23634ce113c0a1e466c578ac57e5b4a07159f
Alfon <Alfon22A@hotmail.com>
2022-08-26 09:40:53 +0200

    Version 5.1

--- App/Spot-me-songs.py
+++ App/Spot-me-songs.py
@@ -30,23 +30,33 @@ def main ():
 			def search_song(song):
 	
 				sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id= Client_ID, client_secret= Client_Secret))
+					
 				results = sp.search(q=song, limit = 5)
-			
-				st.write("Pick the one you meant:")
-    
-				for track in results["tracks"]["items"]:
-					selection = st.button("{} by {} from {}".format(track["name"], track["artists"][0]["name"], track["album"]["name"]))
-					url = track["external_urls"]["spotify"]
-					url2 = url.split("m/t")
-					embed = url2[0]+"m/embed/t"+url2[1]
-					spotify = '<iframe style="border-radius:12px" src={} width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'.format(embed)
-					st.markdown(spotify, unsafe_allow_html=True)
+				
+				if len(results["tracks"]["items"]) > 0:
 					
-					if selection:
-						recommender(track)
+					st.write("Pick the one you meant:")
 					
-				st.write("If it is not there, try again!")
-
+					try:
+						for track in results["tracks"]["items"]:
+							selection = st.button("{} by {} from {}".format(track["name"], track["artists"][0]["name"], track["album"]["name"]))
+							url = track["external_urls"]["spotify"]
+							url2 = url.split("m/t")
+							embed = url2[0]+"m/embed/t"+url2[1]
+							spotify = '<iframe style="border-radius:12px" src={} width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'.format(embed)
+							st.markdown(spotify, unsafe_allow_html=True)
+						
+							if selection:
+								recommender(track)
+							
+					except:
+						pass
+					
+					st.write("If it is not there, try again!")
+				
+				else:
+					st.write("We couldn't find your song, please try again!")
+					
 			search_song(song)
 			
 	else:
